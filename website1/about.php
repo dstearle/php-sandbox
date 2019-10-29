62eab1a1c0e0edc7a6ec8079ae201cc8f4eb348a
dstearle <dallas.st.earle@gmail.com>
2019-10-29 02:06:37 -0700

    PHP Front To Back [Part 9] - Include & Require

---
+++ website1/about.php
@@ -0,0 +1,10 @@
+<!-- Header Component -->
+<?php require 'inc/header.php'; ?>
+
+<!-- Page Title -->
+<h1>About</h1>
+
+<!-- Footer Component -->
+<?php require 'inc/footer.php'; ?>
+
+<!-- Note: Require is like include but will not show the page if the file is missing -->
---
+++ website1/contact.php
@@ -0,0 +1,8 @@
+<!-- Header Component -->
+<?php include 'inc/header.php'; ?>
+
+<!-- Page Title -->
+<h1>Contact</h1>
+
+<!-- Footer Component -->
+<?php include 'inc/footer.php'; ?>
